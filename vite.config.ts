cca31869440d2e77ea4d4f43c6e248fa1672cdcd
Diego Alexandre Rodrigues <diegorodrigues290@hotmail.com>
2024-01-26 21:55:50 -0300

    chore: move environment variables to a specific folder and configure them in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import EnvironmentPlugin from "vite-plugin-environment";
 
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    plugins: [react()],
-
+    plugins: [react(), EnvironmentPlugin('all', { prefix: 'VITE_APP_' })],
+    envDir: "./environments",
     define: {
-      'process.env': process.env
+      'process.env': process.env,
     }
   });
 };
